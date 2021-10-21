35389256afce6e2a0083c7b377d8b2cdfc8b9d7f
liuyu <liuyuit@aliyun.com>
2021-10-21 21:54:27 +0800

    feature: defer

--- 7-defer/defer.go
+++ 7-defer/defer.go
@@ -3,10 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	//写入defer关键字
+	//写入defer关键字, 会在 defer 所在函数结束前触发
 	defer fmt.Println("main end1")
 	defer fmt.Println("main end2")
-
 
 	fmt.Println("main::hello go 1")
 	fmt.Println("main::hello go 2")
